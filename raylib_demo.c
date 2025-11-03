d6676d8ea2e6642577b6fa7e4287f1c4fdd7cb2b
Niklas Malthouse <102893782+nmalthouse@users.noreply.github.com>
2025-11-02 21:20:42 -0600

    fuzz

--- raylib_demo.c
+++ raylib_demo.c
@@ -4,25 +4,39 @@
 #include <stdlib.h>
 
 void main(){
+    int scrw = 800;
+    int scrh = 600;
     printf("Hello\n");
-    InitWindow(800,600, "get spooky");
+    InitWindow(scrw,scrh, "get spooky");
     SetTargetFPS(60);
 
     bumpc_Ctx bctx = bumpc_init();
 
-    size_t rect_ct = 30;
+    size_t rect_ct = 50;
     bumpc_Aabb * rects  = malloc(sizeof(bumpc_Aabb) * rect_ct);
     for(int i = 0; i < rect_ct; i++){
-        rects[i].pos = bumpc_VecNew(rand() % 800, rand() % 600);
+        rects[i].pos = bumpc_VecNew(rand() % scrw, rand() % scrh);
         rects[i].ext = bumpc_VecNew(rand() % 100 + 20, rand() % 100 + 20);
     }
 
+    //aligned rects
     rects[0].pos = bumpc_VecNew(40,40);
-
     rects[0].ext = bumpc_VecNew(40,40);
     rects[1].ext = bumpc_VecNew(40,40);
-
     rects[1].pos = bumpc_VecNew(40 * 2,40);
+
+    int bw = 10;
+    rects[2].pos = bumpc_VecNew(-bw,0);
+    rects[2].ext = bumpc_VecNew(bw, scrh);
+
+    rects[3].pos = bumpc_VecNew(0,-bw);
+    rects[3].ext = bumpc_VecNew(scrw, bw);
+
+    rects[4].pos = bumpc_VecNew(0,scrh);
+    rects[4].ext = bumpc_VecNew(scrw, bw);
+
+    rects[5].pos = bumpc_VecNew(scrw,0);
+    rects[5].ext = bumpc_VecNew(bw, scrh);
 
     bumpc_Aabb player = {0};
     player.ext.data[0] =  30;
@@ -33,9 +47,20 @@ void main(){
         BeginDrawing();
         ClearBackground(BLACK);
 
+        if(player.pos.data[0] < -bw || player.pos.data[1] < -bw  || player.pos.data[0] > scrw || player.pos.data[1] > scrh){
+            player.pos = bumpc_VecNew(0,0);
+        }
+
         float dx = 0;
         float dy = 0;
+        bool dorand = true;
 
+        float mul = 100;
+        if(dorand){
+            dx += rand() % 3 - 1;
+            dy += rand() % 3 - 1;
+        }
+        else{
         if (IsKeyDown(KEY_D))
             dx += 1;
         if (IsKeyDown(KEY_A))
@@ -44,9 +69,13 @@ void main(){
             dy += 1;
         if (IsKeyDown(KEY_W))
             dy -= 1;
+        }
+
+        if(IsKeyDown(KEY_SPACE))
+            player.pos.data[0] += 10;
         
-        dx *= 1.5;
-        dy *= 1.5;
+        dx *= mul;
+        dy *= mul;
 
         for(int i = 0; i < rect_ct; i++){
             const bumpc_Aabb re = rects[i];
@@ -58,29 +87,31 @@ void main(){
         bumpc_Vec goal = bumpc_VecNew(player.pos.data[0] + dx, player.pos.data[1] + dy);
         bumpc_Aabb moved = player;
 
-        bool loop = true;
-        printf("start\n");
-        while(loop){
+        size_t counter = 0;
+        while(counter < 100){
+            counter += 1;
             size_t num_col = bumpc_detectCollisionList(&bctx, moved, rects, rect_ct, goal);
             //TODO prevent infinite loop
             if(num_col == 0)
                 break;
             for(int i = 0; i < num_col; i++){
                 bumpc_CollisionResult r  = bctx.output[i];
+                if(r.overlaps)
+                    printf("overlapped\n");
 
                 moved.pos = r.touch;
                 if(r.norm.data[1] != 0){
-                    printf("y %d\n", r.id);
                     goal.data[1]  = r.touch.data[1];
                     break;
                 }
                 else {
-                    printf("x %d\n", r.id);
                     goal.data[0]  = r.touch.data[0];
                     break;
                 }
             }
         }
+        if(counter > 90)
+            printf("maxed\n");
         player.pos = goal;
 
         EndDrawing();
